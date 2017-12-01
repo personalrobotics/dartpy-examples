b947c8b079a41dc6075a7360758ae39497e0f0f5
Jeongseok Lee <jslee02@gmail.com>
2017-11-30 17:39:17 -0800

    Add urdf parser test

---
+++ tests/test_UrdfParser.py
@@ -0,0 +1,22 @@
+import dartpy as dart
+import numpy as np
+import os.path
+import sys
+import unittest
+
+class TestUrdfParser(unittest.TestCase):
+    def test_parse_skeleton(self):
+        root_path = os.path.join(os.path.dirname(__file__), 'data')
+        cube_path = os.path.join(root_path, 'urdf', 'cube.urdf')
+
+        urdf_loader = dart.utils.DartLoader()
+        cube = urdf_loader.parseSkeleton(cube_path)
+
+        self.assertIsNotNone(cube)
+        self.assertEqual(cube.getNumBodyNodes(), 1)
+        self.assertEqual(cube.getNumJoints(), 1)
+        self.assertEqual(cube.getNumDofs(), 6)
+        self.assertIsNotNone(cube.getRootBodyNode(0))
+
+if __name__ == '__main__':
+    unittest.main()
